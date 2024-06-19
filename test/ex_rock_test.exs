ebb3996416c987c233cd5bcd78e31f9f3342c0b1
Maxim Molchanov <mmolchanov@vonmo.com>
2024-06-20 01:32:31 +0300

    added first test

--- test/ex_rock_test.exs
+++ test/ex_rock_test.exs
@@ -2,7 +2,30 @@ defmodule ExRockTest do
   use ExUnit.Case
   doctest ExRock
 
-  test "greets the world" do
-    assert ExRock.hello() == :world
+  describe "common" do
+    test "check lxcode/0 returns" do
+      assert ExRock.lxcode() == {:ok, :vsn1}
+    end
+  end
+
+  describe "atomic" do
+  end
+
+  describe "iterator" do
+  end
+
+  describe "cf" do
+  end
+
+  describe "snapshot" do
+  end
+
+  describe "checkpoint" do
+  end
+
+  describe "backup" do
+  end
+
+  describe "perf" do
   end
 end
