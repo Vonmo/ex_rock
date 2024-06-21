b80385dde65567e7270d910dcb4be81696fa053d
Maxim Molchanov <mmolchanov@vonmo.com>
2024-06-22 00:00:08 +0300

    added checkpoint test

--- test/checkpoint_test.exs
+++ test/checkpoint_test.exs
@@ -2,5 +2,18 @@ defmodule ExRock.Checkpoint.Test do
   use ExRock.Case, async: true
 
   describe "checkpoint" do
+    test "create_checkpoint", context do
+      path = context.db_path
+      cp_path = path <> "_cp"
+      ExRock.destroy(path)
+      ExRock.destroy(cp_path)
+
+      {:ok, db} = ExRock.open(path)
+      :ok = ExRock.put(db, "k0", "v0")
+      :ok = ExRock.create_checkpoint(db, cp_path)
+
+      {:ok, backup_db} = ExRock.open(cp_path)
+      {:ok, "v0"} = ExRock.get(backup_db, "k0")
+    end
   end
 end
